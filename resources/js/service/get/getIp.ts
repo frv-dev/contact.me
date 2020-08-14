aa6ea60436369cfa3add0fd093ef26957fc59885
Felipe Renan <feliperenanvieira@gmail.com>
2020-08-14 00:32:30 -0300

    Adding getIp return

--- resources/js/service/get/getIp.ts
+++ resources/js/service/get/getIp.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import ipRegex from "../../regex/ipRegex";
 
-export default async function () {
+export default async function (): Promise<string> {
   try {
     const response = await Axios.get<string>('https://www.cloudflare.com/cdn-cgi/trace');
     const result = response.data.match(ipRegex);
